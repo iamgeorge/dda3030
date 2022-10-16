5b1c1dc7e99dc792d6a0fc237dbe0fb03ba95abd
iamgeorge <wangxiiq200@icloud.com>
2022-10-16 13:58:12 +0000

    2.2 init

---
+++ src/2_2.py
@@ -0,0 +1,25 @@
+from sklearn import datasets
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import pandas as pd
+# loading the iris dataset
+iris = pd.read_excel("/home/george/dda3030/src/Classification iris.xlsx")
+# X -> features, y -> label
+X = iris.data
+y = iris.target
+print(X)
+print(y)
+# dividing X, y into train and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+  
+# training a KNN classifier
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 7).fit(X_train, y_train)
+  
+# accuracy on X_test
+accuracy = knn.score(X_test, y_test)
+print(accuracy)
+  
+# creating a confusion matrix
+knn_predictions = knn.predict(X_test) 
+cm = confusion_matrix(y_test, knn_predictions)
